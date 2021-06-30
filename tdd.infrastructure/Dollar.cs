8e5611e6e7798b5d57383323b8acf8640bca669d
Devin Lambert <dlambert@oattravel.com>
2021-06-30 08:34:09 -0400

    Removing Duplication - short hand multiplication

--- tdd.infrastructure/Dollar.cs
+++ tdd.infrastructure/Dollar.cs
@@ -12,7 +12,7 @@ namespace tdd.infrastructure
 
         public void times(int multiplier)
         {
-            amount = amount * multiplier;
+            amount *= multiplier;
         }
     }
 }
