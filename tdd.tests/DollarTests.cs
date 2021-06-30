2fb65fb977fa89a2e18b8852962e181388ecd6a3
Devin Lambert <dlambert@oattravel.com>
2021-06-30 08:22:42 -0400

    Added first test, testMultiplication. Updated to-dos

---
+++ tdd.tests/DollarTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace tdd.tests
+{
+    [TestClass]
+    public class DollarTests
+    {
+        [TestMethod]
+        public void testMultiplication()
+        {
+            DollarTests five = new Dollar(5);
+            five.times(2);
+            Assert.AreEqual(10, five.amount);
+        }
+    }
+}
