38605d4209850120f4690d5bc4e9f345250f4099
Devin Lambert <dlambert@oattravel.com>
2021-07-26 22:06:35 -0400

    switching assertion to compare objects rather than amount property

--- tdd.tests/DollarTests.cs
+++ tdd.tests/DollarTests.cs
@@ -11,7 +11,7 @@ namespace tdd.tests
         {
             Dollar five = new Dollar(5);
             Dollar product = five.times(2);
-            Assert.AreEqual(10, product.amount);
+            Assert.AreEqual(new Dollar(10), product);
             product = five.times(3);
             Assert.AreEqual(15, product.amount);
         }
