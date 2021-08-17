079b2fe208ca0ef4f0ada53bf369f46ba46d8a88
Devin Lambert <dlambert@oattravel.com>
2021-08-17 19:14:58 -0400

    wrote a failing test

---
+++ tdd.tests/FrancTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace tdd.tests
+{
+    [TestClass]
+    public class FrancTests
+    {
+        [TestMethod]
+        public void testFrancMultiplication()
+        {
+            Franc five = new Franc(5);
+            Assert.Equals(new Franc(10), five.times(2));
+            Assert.Equals(new Franc(15), five.times(3));
+        }
+    }
+}
