13f798b207e12c116c76ee72d9107e96707e14f4
Devin Lambert <dlambert@oattravel.com>
2021-08-17 22:23:23 -0400

    write failing test

--- tdd.tests/DollarTests.cs
+++ tdd.tests/DollarTests.cs
@@ -19,6 +19,8 @@ namespace tdd.tests
         {
             Assert.IsTrue(new Dollar(5).Equals(new Dollar(5)));
             Assert.IsFalse(new Dollar(5).Equals(new Dollar(6)));
+            
+            Assert.IsFalse(new Franc(5).Equals(new Dollar(5)));
         }
     }
 }
