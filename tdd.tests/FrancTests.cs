d6ac858e1633c28483fea1a4ee3ad3ab98a77212
Devin Lambert <dlambert@oattravel.com>
2021-08-17 22:17:10 -0400

    added equality test to francTests in preparation to remove franc equals()

--- tdd.tests/FrancTests.cs
+++ tdd.tests/FrancTests.cs
@@ -13,5 +13,12 @@ namespace tdd.tests
             Assert.AreEqual(new Franc(10), five.times(2));
             Assert.AreEqual(new Franc(15), five.times(3));
         }
+
+        [TestMethod]
+        public void testEquality()
+        {
+            Assert.IsTrue(new Franc(5).Equals(new Franc(5)));
+            Assert.IsFalse(new Franc(5).Equals(new Franc(6)));
+        }
     }
 }
